7ef8fbea3d6cc7b0043b8b0f68d62408d038d4c7
EloiGry <eloi.grychta@gmail.com>
2022-01-03 15:38:12 +0100

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 const port = 5000
 const session = require("express-session")
 const passport = require("./config/passport")
@@ -8,6 +9,7 @@ const adminRoutes = require("./routes/admin")
 
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.use(session({
     secret : "secret",
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,61 @@
 const express = require("express")
 const app = express()
 const passport = require("../config/passport")
+const fs = require("fs")
+const users = require("../users.json")
+
+const path = "./users.json"
+
+app.get('/signup', (req, res) => {
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            res.status(500).send("Internal server error")
+        }
+
+        const messages = Json.parse(data)
+        res.json(messages)
+    })
+})
+
+
+
+app.post('/signup', (req, res) => {
+
+const emailVerification = users.find(user => user.email === email)
+const usernameVerification = users.find(user => user.username === username )
+
+    if (!emailVerification && !usernameVerification) {
+        const allIds = users.map(user => user.id)
+        const biggestId = Math.max(...allIds)
+        
+        const message = {
+            id : biggestId + 1,
+            ...req.body
+        }
+
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                console.log("error", err)
+                res.status("500").send("Interval server error");
+            }
+
+            let messages = JSON.parse(data)
+            messages = [...messages, message]
+
+            fs.writeFile(path, JSON.stringify(messages), (err) => {
+                if (err) {
+                    res.status(500).send("Interval server error")
+                }
+            })
+        })
+
+        res.json(message)
+    } else {
+        res.status(409).send("Username or Email already exists")
+    }
+
+
+})
 
 app.post('/login', passport.authenticate("local"), (req, res) => {
     if (req.user) {
